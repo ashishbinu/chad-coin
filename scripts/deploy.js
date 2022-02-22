3a010c2e6161f522e38577ca715e7b8ad4084a09
ashishbinu <ashishbinu90@gmail.com>
2022-02-22 20:39:47 +0530

    🐛 fix the token supply

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,12 @@
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const coinSupply = "69420000000" + "".padEnd(18, "0");
+
   const ChadTokenFactory = await hre.ethers.getContractFactory("ERC20Token");
-  const chadTokenContract = await ChadTokenFactory.deploy("69420000000", "Chad", "CHD");
+  const chadTokenContract = await ChadTokenFactory.deploy(coinSupply, "Chad", "CHD");
   await chadTokenContract.deployed();
 
   console.log("ChadToken :", chadTokenContract.address);
